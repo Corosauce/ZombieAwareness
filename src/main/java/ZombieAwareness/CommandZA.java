869e68cd0a88b49b794b2137800f72abf314d4c6
Mitchell Skaggs <skaggsm333@gmail.com>
2017-07-16 21:36:26 -0500

    Added Gradle Wrapper 4.1-M1, Updated MC to 1.12, updated ForgeGradle to 2.3, updated CoroUtil to 1.12-1.1.13

--- src/main/java/ZombieAwareness/CommandZA.java
+++ src/main/java/ZombieAwareness/CommandZA.java
@@ -1,34 +1,33 @@
 package ZombieAwareness;
 
+import CoroUtil.OldUtil;
+import CoroUtil.pathfinding.PFQueue;
+import CoroUtil.util.CoroUtilMisc;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
-import CoroUtil.OldUtil;
-import CoroUtil.pathfinding.PFQueue;
-import CoroUtil.util.CoroUtilMisc;
 
 public class CommandZA extends CommandBase {
 
 	@Override
-	public String getCommandName() {
+	public String getName() {
 		return "za";
 	}
 
 	@Override
-	public String getCommandUsage(ICommandSender icommandsender) {
+	public String getUsage(ICommandSender icommandsender) {
 		return "Magic dev method!";
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender var1, String[] var2) {
-		
+
 		EntityPlayer player = null;
 		if (var1 instanceof EntityPlayer) {
 			player = (EntityPlayer) var1;
@@ -37,7 +36,7 @@ public class CommandZA extends CommandBase {
 		int dimension = world.provider.getDimension();
 		BlockPos posBlock = var1.getPosition();
 		Vec3d posVec = var1.getPositionVector();
-		
+
 		try {
 			if (var2.length < 1)
 	        {
@@ -47,7 +46,7 @@ public class CommandZA extends CommandBase {
 	        }
 	        else
 	        {
-	        	
+
 	        	if (var2[0].equalsIgnoreCase("set")) {
 	        		if (var2[1].equalsIgnoreCase("PFQueue")) {
 	        			PFQueue.instance = null;
@@ -57,19 +56,19 @@ public class CommandZA extends CommandBase {
 			        		intVal = Integer.valueOf(var2[2]);
 			        	} catch (Exception ex2) { } //silence!
 			        	boolean boolVal = Boolean.valueOf(var2[2]);
-			        	
+
 			        	if (intVal > 0) {
 			        		OldUtil.setPrivateValueBoth(ZombieAwareness.class, ZombieAwareness.instance, var2[1], var2[1], intVal);
 			        	} else {
 			        		OldUtil.setPrivateValueBoth(ZombieAwareness.class, ZombieAwareness.instance, var2[1], var2[1], boolVal);
 			        	}
-			        	
+
 			        	CoroUtilMisc.sendCommandSenderMsg(var1, var2[1] + " now set to: " + OldUtil.getPrivateValueBoth(ZombieAwareness.class, ZombieAwareness.instance, var2[1], var2[1]));
 	        		}
-		        	
-		        	
+
+
 		        	//System.out.println("new setting for max zombies: " + ZombieAwareness.maxZombiesNight);
-		        	
+
 		        	//if (var2[0] == "maxZombiesNight") ZombieAwareness.maxZombiesNight = Integer.valueOf(var2[1]);
 	        	} else if (var2[0].equalsIgnoreCase("get")) {
 	        		if (var2[1].equalsIgnoreCase("time")) {
@@ -84,25 +83,25 @@ public class CommandZA extends CommandBase {
 	        			CoroUtilMisc.sendCommandSenderMsg(var1, var2[1] + " set to: " + OldUtil.getPrivateValueBoth(ZombieAwareness.class, ZombieAwareness.instance, var2[1], var2[1]));
 	        		}
 				} else if (var2[0].equalsIgnoreCase("isActive")) {
-					var1.addChatMessage(new TextComponentString((ZAUtil.isZombieAwarenessActive(world) ? "true" : "false")));
+					var1.sendMessage(new TextComponentString((ZAUtil.isZombieAwarenessActive(world) ? "true" : "false")));
 				} else if (var2[0].equalsIgnoreCase("profile") && player != null) {
 					ZAUtil.startProfile(player.getName());
-					player.addChatComponentMessage(new TextComponentString("ZA Profile started"));
+					player.sendMessage(new TextComponentString("ZA Profile started"));
 				}
-	        	
+
 	        }
 		} catch (Exception ex) {
 			System.out.println("Caught ZA command crash!!!");
 			ex.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender par1ICommandSender)
     {
-        return par1ICommandSender.canCommandSenderUseCommand(this.getRequiredPermissionLevel(), this.getCommandName());
+        return par1ICommandSender.canUseCommand(this.getRequiredPermissionLevel(), this.getName());
     }
-	
+
 	@Override
 	public int getRequiredPermissionLevel() {
 		return 2;
--- src/main/java/ZombieAwareness/CommonProxy.java
+++ src/main/java/ZombieAwareness/CommonProxy.java
@@ -2,6 +2,7 @@ package ZombieAwareness;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.IGuiHandler;
@@ -20,7 +21,7 @@ public class CommonProxy implements IGuiHandler
     public void init(ZombieAwareness pMod)
     {
         mod = pMod;
-    	EntityRegistry.registerModEntity(EntityScent.class, "EntityScent", entityId++, pMod, 32, 20, false);
+    	EntityRegistry.registerModEntity(new ResourceLocation(ZombieAwareness.modID,"EntityScent"), EntityScent.class, "EntityScent", entityId++, pMod, 32, 20, false);
     }
 
     public int getUniqueTextureLoc()
@@ -63,7 +64,7 @@ public class CommonProxy implements IGuiHandler
     {
         if (FMLCommonHandler.instance().getMinecraftServerInstance() != null)
         {
-            return FMLCommonHandler.instance().getMinecraftServerInstance().worldServerForDimension(0);
+            return FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(0);
         }
         else
         {
@@ -73,7 +74,7 @@ public class CommonProxy implements IGuiHandler
 
     public World getSidesWorld()
     {
-        return FMLCommonHandler.instance().getMinecraftServerInstance().worldServerForDimension(0);
+        return FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(0);
     }
 
     @Override
@@ -92,14 +93,14 @@ public class CommonProxy implements IGuiHandler
 
     public void weatherDbg()
     {
-    	
+
     }
 
     public void windDbg()
     {
-    	
+
     }
-    
+
     public Entity getEntByID(int id) {
 		System.out.println("common getEntByID being used, this is bad");
 		return null;
