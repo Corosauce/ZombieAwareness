db926bdbd7b2ce87ce9d1d485feb58cdedf84a1d
Corosauce <cordonfreeman@gmail.com>
2022-01-12 20:47:41 +0000

    Use a less invasive mixin method, fixes https://github.com/Corosauce/ZombieAwareness/issues/58 , add lazydfu because it makes life better

--- src/main/java/com/corosus/zombieawareness/mixin/MixinLevelEvent.java
+++ src/main/java/com/corosus/zombieawareness/mixin/MixinLevelEvent.java
@@ -1,33 +1,20 @@
 package com.corosus.zombieawareness.mixin;
 
 import com.corosus.zombieawareness.ZAUtil;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundLevelEventPacket;
-import net.minecraft.server.players.PlayerList;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import javax.annotation.Nullable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLevel.class)
-public class MixinLevelEvent {
+public abstract class MixinLevelEvent {
 
-    @Redirect(method = "levelEvent",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/server/players/PlayerList;broadcast(Lnet/minecraft/world/entity/player/Player;DDDDLnet/minecraft/resources/ResourceKey;Lnet/minecraft/network/protocol/Packet;)V"))
-    public void broadcast(PlayerList playerList, @Nullable Player pExcept, double pX, double pY, double pZ, double pRadius, ResourceKey<Level> pDimension, Packet<?> pPacket) {
-        playerList.broadcast(pExcept, pX, pY, pZ, pRadius, pDimension, pPacket);
-
-        ClientboundLevelEventPacket packet = (ClientboundLevelEventPacket) pPacket;
-        int type = packet.type;
-        Level world = playerList.getServer().getLevel(pDimension);
-        if (world != null) {
-            ZAUtil.hookPlayEvent(type, world, pX, pY, pZ, 0);
-        }
+    @Inject(method = "levelEvent",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcast(Lnet/minecraft/world/entity/player/Player;DDDDLnet/minecraft/resources/ResourceKey;Lnet/minecraft/network/protocol/Packet;)V"))
+    public void injectBroadcast(Player p_8684_, int type, BlockPos blockPos, int data, CallbackInfo ci) {
+        ZAUtil.hookPlayEvent(type, ((ServerLevel)(Object)this), blockPos.getX(), blockPos.getY(), blockPos.getZ(), data);
     }
 }
--- src/main/java/com/corosus/zombieawareness/mixin/MixinPlaySound.java
+++ src/main/java/com/corosus/zombieawareness/mixin/MixinPlaySound.java
@@ -1,34 +1,21 @@
 package com.corosus.zombieawareness.mixin;
 
 import com.corosus.zombieawareness.ZAUtil;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundSoundPacket;
-import net.minecraft.server.players.PlayerList;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.Level;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import javax.annotation.Nullable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLevel.class)
-public class MixinPlaySound {
+public abstract class MixinPlaySound {
 
-    @Redirect(method = "playSound",
-    at = @At(value = "INVOKE",
-    target = "Lnet/minecraft/server/players/PlayerList;broadcast(Lnet/minecraft/world/entity/player/Player;DDDDLnet/minecraft/resources/ResourceKey;Lnet/minecraft/network/protocol/Packet;)V"))
-    public void broadcast(PlayerList playerList, @Nullable Player pExcept, double pX, double pY, double pZ, double pRadius, ResourceKey<Level> pDimension, Packet<?> pPacket) {
-        playerList.broadcast(pExcept, pX, pY, pZ, pRadius, pDimension, pPacket);
-
-        ClientboundSoundPacket packet = (ClientboundSoundPacket) pPacket;
-        SoundEvent sound = packet.sound;
-        Level world = playerList.getServer().getLevel(pDimension);
-        if (world != null) {
-            ZAUtil.hookSoundEvent(sound, world, pX, pY, pZ, 1, 1);
-        }
+    @Inject(method = "playSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcast(Lnet/minecraft/world/entity/player/Player;DDDDLnet/minecraft/resources/ResourceKey;Lnet/minecraft/network/protocol/Packet;)V"))
+    public void injectBroadcast(Player pExcept, double pX, double pY, double pZ, SoundEvent sound, SoundSource soundSource, float volume, float pitch, CallbackInfo ci) {
+        ZAUtil.hookSoundEvent(sound, ((ServerLevel)(Object)this), pX, pY, pZ, volume, pitch);
     }
 }
